76cbec50e6054aea3e8df013b4c02eedc1ef3d2e
Rasbandit <Rasbandit@users.noreply.github.com>
2026-05-28 23:02:13 -0600

    chore: bump version to 0.5.246

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Engram.MixProject do
   def project do
     [
       app: :engram,
-      version: "0.5.245",
+      version: "0.5.246",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
