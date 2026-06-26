759147a902152b36a1c3b953f658b3550851e8dc
Rasbandit <Rasbandit@users.noreply.github.com>
2026-06-26 17:41:41 -0600

    chore(crdt-diag): bump version for diag branch push

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Engram.MixProject do
   def project do
     [
       app: :engram,
-      version: "0.5.546",
+      version: "0.5.547",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
