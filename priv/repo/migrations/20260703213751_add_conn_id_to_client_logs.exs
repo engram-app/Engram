e3c682a22d232de84f91481dfd2521037c153ea1
Rasbandit <Rasbandit@users.noreply.github.com>
2026-07-04 01:14:58 -0600

    fix(migration): use text not varchar for conn_id/device_id (squawk)

--- priv/repo/migrations/20260703213751_add_conn_id_to_client_logs.exs
+++ priv/repo/migrations/20260703213751_add_conn_id_to_client_logs.exs
@@ -3,8 +3,8 @@ defmodule Engram.Repo.Migrations.AddConnIdToClientLogs do
 
   def change do
     alter table(:client_logs) do
-      add :conn_id, :string
-      add :device_id, :string
+      add :conn_id, :text
+      add :device_id, :text
     end
   end
 end
